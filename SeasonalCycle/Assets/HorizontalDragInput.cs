5c14a3e2a250900e664f60f55d0718429fa12d6b
Taijj <tobias.burger@bonuslevel.com>
2025-01-27 16:37:51 +0100

    Implemented Input

---
+++ SeasonalCycle/Assets/HorizontalDragInput.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HorizontalDragInput : MonoBehaviour, IDragHandler
+{
+    public event Action<float> OnHorizontalDrag;
+
+    [SerializeField, Range(0.01f, 1f)] private float _sensitivity;
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.delta.x == 0f)
+            return;
+
+        OnHorizontalDrag?.Invoke(eventData.delta.x * _sensitivity);
+    }
+}
--- SeasonalCycle/Assets/MainLogic.cs
+++ SeasonalCycle/Assets/MainLogic.cs
@@ -1,27 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.Splines;
 
 public class MainLogic : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    #region Fields
+    [SerializeField] private Transform _seasonsContainer;
+    [SerializeField] private Transform[] _seasons;
+    [SerializeField] private Spline _spline;
+    [SerializeField] private HorizontalDragInput _input;
+    [Space]
+    [SerializeField, ReadOnly(true), Range(0f, 1f)]
+    private float _parameter;
+    #endregion
     
     
+    
+    #region Input
     public void Awake()
     {
-        _splineContainer = GetComponentInChildren<SplineContainer>();
+        _input.OnHorizontalDrag += OnDrag;
     }
 
-    private float _parameter;
-    private SplineContainer _splineContainer;
-    
-    public void Update()
+    public void OnDestroy()
     {
-        _parameter += Time.deltaTime;
-        if (_parameter > 1f)
-            _parameter -= 1f;
-        
-        _target.position = _splineContainer.Spline.EvaluatePosition(_parameter);
+        _input.OnHorizontalDrag -= OnDrag;
     }
+
+    private void OnDrag(float xDelta)
+    {
+        _parameter += xDelta;
+    }
+    #endregion
+
+    
+
+#if UNITY_EDITOR
+    public void OnValidate()
+    {
+        _spline = GetComponentInChildren<SplineContainer>().Spline;
+
+        _seasons = new Transform[_seasonsContainer.childCount];
+        for(int i = 0; i < _seasonsContainer.childCount; i++)
+            _seasons[i] = _seasonsContainer.GetChild(i);
+
+        _input = GetComponentInChildren<HorizontalDragInput>();
+    }
+#endif
 }
